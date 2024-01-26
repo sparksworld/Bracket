611596468f0de1d71b58fd38cb6f50102e2ac830
spark <spark.xiaoyu@qq.com>
2024-01-26 17:45:58 +0800

    add logo

---
+++ lib/widgets/update.dart
@@ -0,0 +1,34 @@
+import 'package:bracket/plugins.dart';
+
+class Update extends ModalRoute {
+  @override
+  // TODO: implement barrierColor
+  Color? get barrierColor => throw UnimplementedError();
+
+  @override
+  // TODO: implement barrierDismissible
+  bool get barrierDismissible => throw UnimplementedError();
+
+  @override
+  // TODO: implement barrierLabel
+  String? get barrierLabel => throw UnimplementedError();
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    // TODO: implement buildPage
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement maintainState
+  bool get maintainState => throw UnimplementedError();
+
+  @override
+  // TODO: implement opaque
+  bool get opaque => throw UnimplementedError();
+
+  @override
+  // TODO: implement transitionDuration
+  Duration get transitionDuration => throw UnimplementedError();
+}
